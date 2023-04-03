2f18a84f7a7493cff57c0780d789bc35a253e68f
Lizandro Ruiz <lizandro.bresso@gmail.com>
2023-04-02 19:43:30 -0500

    js module

---
+++ JS/Operators.js
@@ -0,0 +1,34 @@
+// Lista de operadores y su función
+
+// suma
+console.log(15+12); 
+
+// resta
+console.log(18-16);
+
+// Multiplicacion
+console.log(15*2);
+
+// Division
+console.log(14/2);
+
+// modulo, el módulo divide y muestra el restante de la divisón al inter mas cercanao
+console.log(18 % 8);
+
+//Operaciones de comparación
+console.log (4 < 7); 
+console.log (4 < 1 );
+console.log (4 > 4); 
+console.log (4 == 7);
+
+//Operaciones de comparación
+console.log (4 < 7); 
+console.log (4 < 1 );
+console.log (4 > 4); 
+console.log (4 == 7);
+
+//Operaciones de comparación
+console.log (4 < 7); 
+console.log (4 < 1 );
+console.log (4 > 4); 
+console.log (4 == 7);
---
+++ JS/Variables.js
@@ -0,0 +1,15 @@
+// Strings 
+var bootcamp = 'Henry'
+
+// Numeros or integer values
+var numeroEnter = 23
+
+// Booleans 
+var Llueve = false
+
+// Undefined 
+var cajon1; 
+console.log(cajon1)
+
+//Null
+var sinNada = null
