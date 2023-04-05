6fbe14bb38bab1edd4215cf25075fb99548d8a49
Lizandro Ruiz <lizandro.bresso@gmail.com>
2023-04-04 22:20:32 -0500

    notes about bucles

---
+++ JS/Bucles.js
@@ -0,0 +1,13 @@
+var suma = 0;
+
+for (var i = 0 ; i < 10 ; i++) {
+    suma = suma + i;
+    console.log(suma);
+}
+// 0, 1, 3, 6, 10,
+
+while (suma < 3){
+    suma = suma + 1;
+    console.log(suma);
+}
+// 1, 2, 3
