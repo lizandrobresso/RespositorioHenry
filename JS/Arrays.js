520511cb0701973c37c659369604f0c67e40dad4
Lizandro Ruiz <lizandro.bresso@gmail.com>
2023-04-05 19:08:06 -0500

    Arrays added

---
+++ JS/Arrays.js
@@ -0,0 +1,51 @@
+// De esta manera se agregan valores a la array
+var listaDeCompras = [];
+listaDeCompras[3] = 'Tomates';
+listaDeCompras[2] = 'Lechuga';
+
+// Hay varias maneras de acceder a las array 
+listaDeCompras[1]; // Esta es una
+var elementoDelArray = listaDeCompras[1]; // Se puede hacer una variable que acceda al elemento que necesitamos
+
+// PUSH y UNSHIFT
+var colores = ['Amarillo', 'Azul'];
+colores.push('Rojo');
+colores.unshift('Verde');
+
+console.log(colores);
+
+// POP y SHIFT
+colores.shift();
+colores.pop();
+
+console.log(colores);
+// INCLUDES
+var pintores = ['Picasso', 'Velázquez', 'Van Gogh', 'Dalí'];
+var existeDali = pintores.includes('Dalí');
+
+console.log(existeDali);
+// EVERY
+var numeros = [ 1, 6, 8, 9, 43 ];
+var cumplenCondicion = numeros.every( ( num ) => { num > 5 } );
+
+console.log(cumplenCondicion);
+// SPLIT
+var palabra = 'Henri';
+var palabraSeparada = palabra.split('');
+
+console.log(palabraSeparada);
+
+// JOIN
+var palabraArreglada = palabraSeparada.join('');
+
+console.log(palabraArreglada);
+
+// FOREACH
+var numeros = [ 1, 2, 3, 4 ];
+numeros.forEach( (num) => { console.log(num) } )
+
+// MAP
+var numeros = [ 1, 2, 3, 4 ];
+var masUno = numeros.map( (num) => { return num + 1 } );
+
+console.log(masUno);
